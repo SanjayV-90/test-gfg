4d1a16e9273bcee30f680a30c5978cccf7b73795
SanjayV-90 <vsanjayv152004@gmail.com>
2025-07-17 20:43:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Third largest element/third-largest-element.java
+++ Difficulty: Easy/Third largest element/third-largest-element.java
@@ -1,39 +1,37 @@
 class Solution {
     int thirdLargest(int arr[]) {
-        int a = arr.length;
         
-        int first = Integer.MIN_VALUE;
-        int second = Integer.MIN_VALUE;
-        int third = Integer.MIN_VALUE;
-        
-        if(a<3)
-            return -1;
-            
-        for (int value : arr)
+        if(arr.length<3)
         {
-            if(value > first)
-            {
-               third = second;
-                second = first;
-                
-                 first = value;
-            }
-            
-            else if(value > second)
-            {
-                
-                third = second;
-                second = value;
-            }
-            
-            else if(value > third)
-            {
-                third = value;
-            }
-            
+            return -1;
         }
         
-        return third;
+        else{
+            
+            int x1=Integer.MIN_VALUE;
+            int x2=Integer.MIN_VALUE;
+            int x3=Integer.MIN_VALUE;
+            
+            for (int i=0 ; i<arr.length ; ++i)
+            {
+                if(x1<arr[i])
+                {
+                    x3=x2;
+                    x2=x1;
+                    x1=arr[i];
+                }
+                else if(x2<arr[i])
+                {
+                     x3=x2;
+                    x2=arr[i];
+                }
+                else if(x3<arr[i])
+                {
+                     x3=arr[i];
+                }
+            }
+            return x3;
+        }
         
     }
 }
